a8629f4bae7b5e3e074066e7df613f8c1efb5e5f
ShweatyWarker <nicholas.gibson@triosstudent.com>
2025-07-18 16:54:14 -0400

    ye

--- Exercise2_8/Exercise2_8.java
+++ Exercise2_8/Exercise2_8.java
@@ -1,5 +1,8 @@
-package Exercise2_8;
+// 1. Display the message "Enter an integer: ", leaving the cursor on the same line
+System.out.print("Enter an integer: ");
 
-public class Exercise2_8 {
-    
-}
+// 2. Assign the product of variables b and c to the int variable a
+int a = b * c;
+
+// 3. Use a comment to state that a program performs a sample payroll calculation
+// This program performs a sample payroll calculation
