af7501c809f458535bd6f67d126be6ac312b2a8d
ShweatyWarker <nicholas.gibson@triosstudent.com>
2025-07-18 17:08:40 -0400

    finished 3rd exercise

---
+++ Exercise2_10/Exercise2_10.java
@@ -0,0 +1,12 @@
+package Exercise2_10;
+
+public class Exercise2_10 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        System.out.printf("x = %d%n", x);
+        System.out.printf("Value of %d + %d is %d%n", x, x, (x + x));
+        System.out.printf("x = %d%n", x);
+        System.out.printf("%d = %d%n", (x + y), (y + x));
+    }
+}
