4ebf96a0d679303c93bf0e2d7a11fa67856f03bc
ShweatyWarker <nicholas.gibson@triosstudent.com>
2025-07-18 17:18:10 -0400

    this is 4

---
+++ Exercise2_18/Exercise2_18.java
@@ -0,0 +1,21 @@
+package Exercise2_18;
+
+public class Exercise2_18 {
+	public static void main(String[] args) {
+		System.out.printf("*%n**%n***%n****%n*****%n");
+	}
+}
+// This program prints a right-angled triangle pattern using asterisks.
+// The output will be:
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// Each line contains one more asterisk than the previous line, creating a triangle shape.
+// The pattern is printed using the printf method, which formats the output with new lines.
+// The first line has one asterisk, the second line has two, and so on, up to five asterisks in the last line.
+// This is a simple example of using loops and formatted output in Java to create a visual pattern.
+// The program demonstrates basic string formatting and output in Java.
