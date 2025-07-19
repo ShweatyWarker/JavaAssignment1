fbf0ce95317e62113d6f7219634a737684a7b543
ShweatyWarker <nicholas.gibson@triosstudent.com>
2025-07-19 12:25:54 -0400

    did answer 3

---
+++ Exercise_2_14/Answer_3.java
@@ -0,0 +1,8 @@
+package Exercise_2_14;
+
+public class Answer_3 {
+    public static void main(String[] args) {
+        // Answer: Using System.out.println
+        System.out.println("1 2 3 4");
+    }
+}
